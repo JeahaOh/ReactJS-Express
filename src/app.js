4fb40ad540c4b843f1577c6555861b5b37dbaef5
JeahaOh <jeaha1217@gmail.com>
2019-01-31 01:40:45 +0900

    react

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//  React Module을 불러와서 사용함.
+
+class App extends Component {
+    //  Component를 만드는 방법중 하나
+  render() {
+      //    render() 메소드. jsx 형태의 
+    return (
+      <div>
+        <h1>안녕하세요 리액트</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
